cb9c43fd5df29f9a68cb87b0b2b27928e8fcb80e
anirban444 <anirban@procifynow.com>
2025-12-12 20:00:36 +0530

    sdafsdaf

---
+++ entity/d_test1.ts
@@ -0,0 +1,6 @@
+import { d_test1 as d_test1_gen } from "sample_app2850/entity_gen/d_test1";
+export class d_test1 extends d_test1_gen {
+	public customFunc(): void {
+		console.log("anirban karmakar");
+	}
+}
