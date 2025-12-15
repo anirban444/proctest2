0d2b52d5ac4bca7e5e9bb6c9576a7bc885c13658
anirban444 <anirban@procifynow.com>
2025-12-15 10:29:29 +0530

    asfdsaf

---
+++ query/q_test1.ts
@@ -0,0 +1,6 @@
+import { q_test1 as q_test1_gen } from "sample_app2850/query_gen/q_test1";
+export class q_test1 extends q_test1_gen {
+	public customFunc(): void {
+		console.log("anirban");
+	}
+}
